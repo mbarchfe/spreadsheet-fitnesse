d937951d7946ed8a7781dc519bc7324e3b716215
mbarchfe <mbarchfe@googlemail.com>
2014-03-20 23:41:59 +0100

    revert accidentally commited change

--- test/acceptance/src/de/markusbarchfeld/spreadsheetfitnesse/AcceptanceTest.java
+++ test/acceptance/src/de/markusbarchfeld/spreadsheetfitnesse/AcceptanceTest.java
@@ -118,9 +118,7 @@ public class AcceptanceTest {
     // as opposed to the non-nested call there is only one sheet, therefore the name of the
     // root page is the file name
     importExcelIntoFitnesse
-        .importFile("test/sample/HelloWorld.xlsx");
+        .importFile(createFullPathToAcceptanceTestData("MacroCallNested.xlsx"));
     assertEquals("2\n", fitnesseRest.getPageContent("MacroCallNested.TestLevel1.TestLevel2"));
   }
-  
-  
 }
