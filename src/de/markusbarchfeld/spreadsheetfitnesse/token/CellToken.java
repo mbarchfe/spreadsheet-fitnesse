73e342b18f34bdab76ac59468dc7a5efe32bb831
mbarchfe <mbarchfe@googlemail.com>
2014-05-28 08:32:08 +0200

    check style

--- src/de/markusbarchfeld/spreadsheetfitnesse/token/CellToken.java
+++ src/de/markusbarchfeld/spreadsheetfitnesse/token/CellToken.java
@@ -5,17 +5,23 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
-
-import sun.security.action.GetBooleanAction;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTCellStyle;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTCellStyles;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTStylesheet;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTXf;
 
 public class CellToken {
   public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yy");
@@ -97,6 +103,35 @@ public class CellToken {
       decimalFormat.setRoundingMode(RoundingMode.HALF_UP);
     }
     return decimalFormat;
+  }
+
+  public Object getStyleName(XSSFWorkbook xssfWorkbook) {
+    CellStyle cellStyle = cell.getCellStyle();
+    if (cellStyle instanceof XSSFCellStyle) {
+      // a very first guess at how to check for styles instead of borders
+      // currently only tested from the acceptance test for RedPencilUsingMacros
+      // basic unit tests needed
+      XSSFCellStyle xssfCellStyle = (XSSFCellStyle) cellStyle;
+      CTXf coreXf = xssfCellStyle.getCoreXf();
+      long xfId = coreXf.getXfId();
+      //XSSFCellStyle cellStyle2 = xssfWorkbook.getCellStyleAt((short)xfId);
+      CTXf cellStyleXfAt = xssfWorkbook.getStylesSource().getCellStyleXfAt((int) xfId);
+      CTStylesheet ctStylesheet = xssfWorkbook.getStylesSource().getCTStylesheet();
+      CTCellStyles cellStyles = ctStylesheet.getCellStyles();
+      List<CTCellStyle> cellStyleList = cellStyles.getCellStyleList();
+      for (CTCellStyle ctCellStyle : cellStyleList) {
+        if (ctCellStyle.getXfId() == xfId) {
+          String name = ctCellStyle.getName();
+          if (ctCellStyle.getCustomBuiltin()) {
+            name = name.replaceAll("Excel Built-in ", "");
+          }
+          return name;
+           
+        }
+      }
+     
+    }
+    return "X";
   }
  
 
